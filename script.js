c1b50a61a6ed66f1e9205fdc0897331b586ace2f
Yuan <yuan291104@gmail.com>
2025-10-08 23:09:34 +0800

    Created toggling between about-me and projects

---
+++ script.js
@@ -0,0 +1,13 @@
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.toggle('active' , t ===tab ));
+
+        // Show matching sections, hide the other
+        const targetID = tab.dataset.target; // target refers to "projects" or "about me" (data-target)
+        contents.forEach(c => c.classList.toggle('is-visible', c.id === targetID));
+    });
+});
+
